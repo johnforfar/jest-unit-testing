123ff5b941c1186abffc1906f5a0d985455642e2
johnforfar <john.forfar@gmail.com>
2023-05-08 21:30:44 +1000

    Matcher testing

--- functions.test.js
+++ functions.test.js
@@ -8,6 +8,28 @@ test('2 + 2 is equal to 4', () => {
     expect(functions.addNum(2, 2)).toEqual(4);
 })
 
+/* Matcher testing
+toBeNull
+toBeUndefined
+toBeDefined
+toBeTruthy
+toBeFalsy
+toBeGreaterThan
+toBeGreaterThanOrEqual
+toBeLessThan
+toBeLessThanOrEqual
+toMatch
+toContain
+toEqual
+toHaveLength
+toMatchObject
+toHaveProperty
+toMatchSnapshot
+toThrow
+toThrowError
+toStrictEqual
+*/
+
 test('object assignment', () => {
     const data = {one: 1};
     data['two'] = 2;
@@ -16,8 +38,18 @@ test('object assignment', () => {
 
 test('is null', () => {
     expect(functions.isNull()).toBeNull();
+    //expect(functions.isNull()).toBeFalsy();
 })
 
 test('is undefined', () => {
     expect(functions.isUndefined()).toBeUndefined();
+    //expect(functions.isUndefined()).toBeFalsy();
+})
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
 })
