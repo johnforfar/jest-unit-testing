63193550235957434e1a04ba2f0eaaf6ee53a6fb
johnforfar <john.forfar@gmail.com>
2023-05-10 18:55:53 +1000

    Added more tests

--- functions.test.js
+++ functions.test.js
@@ -57,3 +57,22 @@ test('zero', () => {
     expect(z).not.toBeTruthy();
     expect(z).toBeFalsy();
 })
+
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+})
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+})
+
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+})
